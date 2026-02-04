d33c0895071b60fc4b8d0f492d5c5d8102c11a8f
Hoshiroo18 <dnelaapri@gmail.com>
2026-02-04 21:07:31 +0700

    JANGAN LUPA PULL FIRST MET

--- resources/views/analisis/rekomendasi-gap.blade.php
+++ resources/views/analisis/rekomendasi-gap.blade.php
@@ -971,7 +971,7 @@
         <div class="row">
             <div class="col-12">
                 <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                    <h4 class="mb-sm-0">{{ $title }}</h4>
+                    <h4 class="mb-sm-0"></h4>
                     <div class="page-title-right">
                         <ol class="breadcrumb m-0">
                             <li class="breadcrumb-item"><a href="{{ route('analisis.index') }}">Analisis</a></li>
